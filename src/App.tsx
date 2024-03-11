4c1029cd20e3e9f395fe79029d4a1772ebe2080c
aluno <aluno@NV-LAB02-135524.ifms.edu.br>
2024-03-11 11:24:28 -0400

    meu primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  let nome = "Vinicius"
+  let sobrenome = "Eduardo"
+
+  return (
+    <>
+      <h1 className="nome">Nome: {nome}</h1>
+      <p className="sobrenome">{sobrenome}</p>
+    </>
+  )
+}
+
+export default App
